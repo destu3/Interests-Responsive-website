d005e911122d3ddb66578a2bc588bf8ce0531b13
destu3 <obose03@gmail.com>
2022-05-26 11:29:42 +0100

    changed size of text on small screens

--- script.js
+++ script.js
@@ -6,8 +6,10 @@ const navArea = document.querySelector(".nav-links")
 
 closeBtn.addEventListener("click",() => {
     navArea.style.right = "-50%"
+    showBtn.style.color = "#231f20"
 })
 
 showBtn.addEventListener("click",() => {
     navArea.style.right = "0"
+    showBtn.style.color = "rgb(148, 81, 126, 0.6 )"
 })
