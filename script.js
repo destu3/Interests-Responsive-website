ca5f3de75a4ffd15fa93a5275a6db05334e4e64d
destu3 <obose03@gmail.com>
2022-05-26 18:16:33 +0100

    added next and prev btn, improved character sect

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 const closeBtn = document.querySelector(".fa-xmark");
 const showBtn = document.querySelector(".fa-bars");
 const navArea = document.querySelector(".nav-links")
+const nextBtn = document.querySelector(".next-btn")
 
 closeBtn.addEventListener("click",() => {
     navArea.style.right = "-50%"
@@ -12,4 +13,8 @@ closeBtn.addEventListener("click",() => {
 showBtn.addEventListener("click",() => {
     navArea.style.right = "0"
     showBtn.style.color = "rgb(148, 81, 126, 0.6 )"
+})
+
+nextBtn.addEventListener("click", () => {
+    console.log("it working")
 })
